ff9ed2f5fe74d8ad50d7d36fa612d0908d2d6f69
Hanna <hannaeberhoefer@yahoo.com>
2020-06-18 22:00:41 +0100

    Add Bootstrap and basic styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
-  validates :username, :email, :password, presence: true
+  validates :username, presence: true, uniqueness: true, length: { in: 4..15 }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :password, presence: true, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/x }
 
 end
